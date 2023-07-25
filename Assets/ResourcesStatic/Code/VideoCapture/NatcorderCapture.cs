0cf7f9368586d263b7f50e75617a77d42aecf67d
Anton <47085893+Batooon@users.noreply.github.com>
2023-07-25 19:04:38 +0300

    video capture implemented

---
+++ Assets/ResourcesStatic/Code/VideoCapture/NatcorderCapture.cs
@@ -0,0 +1,79 @@
+﻿using System.Threading.Tasks;
+using NatML.Recorders;
+using NatML.Recorders.Clocks;
+using UnityEngine;
+
+namespace Code.VideoCapture
+{
+    public class NatcorderCapture
+    {
+        private bool _cameraAvailable;
+        private WebCamTexture _rearCamera;
+
+        private MP4Recorder _recorder;
+        private bool _recording;
+
+        public bool TryInitializeCamera() =>
+            TryFindRearCamera();
+
+        public WebCamTexture GetCameraOutput()
+        {
+            _rearCamera.Play();
+            return _rearCamera;
+        }
+
+        public async void StartRecording()
+        {
+            _recorder = new MP4Recorder(_rearCamera.width, _rearCamera.height, 30);
+
+            _recording = true;
+            var clock = new RealtimeClock();
+            while (_recording)
+            {
+                _recorder.CommitFrame(_rearCamera.GetPixels32(), clock.timestamp);
+
+                await Task.Yield();
+            }
+        }
+
+        public async void StopRecording()
+        {
+            _recording = false;
+
+            var recordingPath = await _recorder.FinishWriting();
+            NativeGallery.Permission permission = NativeGallery.SaveVideoToGallery(recordingPath, "Camera Test",
+                "testvideo.mp4", (success, path) => Debug.Log($"Media save result: {success} {path}"));
+            Debug.Log($"permission result: {permission}");
+        }
+
+        private bool TryFindRearCamera()
+        {
+            WebCamDevice[] devices = WebCamTexture.devices;
+
+            if (devices.Length == 0)
+            {
+                Debug.Log("no camera found");
+                _cameraAvailable = false;
+                return false;
+            }
+
+            foreach (var cameraDevice in devices)
+            {
+                if (cameraDevice.isFrontFacing == false)
+                    _rearCamera = new WebCamTexture();
+            }
+
+            if (_rearCamera == null)
+            {
+                Debug.Log("unable to find rear camera");
+                _cameraAvailable = false;
+                return false;
+            }
+
+            _cameraAvailable = true;
+            Debug.Log("camera found and initialized");
+
+            return _cameraAvailable;
+        }
+    }
+}
---
+++ Assets/ResourcesStatic/Code/VideoCapture/VideoCapture.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Code.VideoCapture
+{
+    public class VideoCapture : MonoBehaviour
+    {
+        [SerializeField] private RawImage _videoOutput;
+        [SerializeField] private AspectRatioFitter _fitter;
+
+        private NatcorderCapture _capture;
+        private WebCamTexture _outputTexture;
+
+        private void Awake() =>
+            _capture = new NatcorderCapture();
+
+        private void Start() => 
+            InitializeCamera();
+
+        public void StartRecording() =>
+            _capture.StartRecording();
+
+        public void StopRecording() =>
+            _capture.StopRecording();
+
+        private void InitializeCamera()
+        {
+            if (_capture.TryInitializeCamera() == false)
+                return;
+            
+            AdjustOutputTexture();
+
+            _outputTexture = _capture.GetCameraOutput();
+            _videoOutput.texture = _outputTexture;
+        }
+
+        private void AdjustOutputTexture()
+        {
+            float ratio = (float)_outputTexture.width / _outputTexture.height;
+            _fitter.aspectRatio = ratio;
+
+            float scaleY = _outputTexture.videoVerticallyMirrored ? -1f : 1f;
+            _videoOutput.rectTransform.localScale = new Vector3(1f, scaleY, 1f);
+
+            int orient = -_outputTexture.videoRotationAngle;
+            _videoOutput.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
+        }
+    }
+}
