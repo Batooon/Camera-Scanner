0ae53be2c1bc1503223635dc929f6d01a101705e
Anton <47085893+Batooon@users.noreply.github.com>
2023-07-25 19:05:15 +0300

    scan button for transitioning to scanner scene implemented

---
+++ Assets/ResourcesStatic/Code/ScanButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Code
+{
+    [RequireComponent(typeof(Button))]
+    public class ScanButton : MonoBehaviour
+    {
+        private const string ScannerScene = "Scanner";
+        private Button _button;
+
+        private void Awake() => 
+            _button = GetComponent<Button>();
+
+        private void Start() => 
+            _button.onClick.AddListener(OpenScanner);
+
+        private void OnDestroy() => 
+            _button.onClick.RemoveListener(OpenScanner);
+
+        private void OpenScanner() => 
+            SceneLoader.LoadScene(ScannerScene);
+    }
+}
