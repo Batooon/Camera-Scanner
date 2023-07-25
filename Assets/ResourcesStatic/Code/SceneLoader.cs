9de8253fc92ff9dc92c9f018265155aa1f8a2b1d
Anton <47085893+Batooon@users.noreply.github.com>
2023-07-25 19:04:53 +0300

    scene loader implemented

---
+++ Assets/ResourcesStatic/Code/SceneLoader.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine.SceneManagement;
+
+namespace Code
+{
+    public static class SceneLoader
+    {
+        public static void LoadScene(string nextScene)
+        {
+            SceneManager.LoadScene(nextScene);
+        }
+    }
+}
